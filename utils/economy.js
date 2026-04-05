d207efc05231621cabe38ef0c6312a38c06e75df
kdfuzzy <kyedow0412@gmail.com>
2026-04-05 22:26:43 +0100

    Create economy.js

---
+++ utils/economy.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const DB_FILE = './data.json';
+
+function getDB() {
+    return JSON.parse(fs.readFileSync(DB_FILE));
+}
+
+function saveDB(data) {
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+}
+
+function getBalance(userId) {
+    const db = getDB();
+    return db[userId]?.balance || 0;
+}
+
+function addBalance(userId, amount) {
+    const db = getDB();
+
+    if (!db[userId]) {
+        db[userId] = { balance: 0 };
+    }
+
+    db[userId].balance += amount;
+    saveDB(db);
+}
+
+function removeBalance(userId, amount) {
+    const db = getDB();
+
+    if (!db[userId]) {
+        db[userId] = { balance: 0 };
+    }
+
+    db[userId].balance -= amount;
+    saveDB(db);
+}
+
+module.exports = {
+    getBalance,
+    addBalance,
+    removeBalance
+};
