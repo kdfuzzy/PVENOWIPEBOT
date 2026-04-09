43dcda358d0a32e0aac2fd33a5ac040ca25509de
kdfuzzy <kyedow0412@gmail.com>
2026-04-09 19:53:47 +0100

    Update addmoney.js

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -1,24 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { addBalance, formatSol } = require('../utils/economy');
 
+const OWNER_ID = '794606718972723230';
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('addmoney')
-        .setDescription('Add SOL to a user')
+        .setDescription('Add money to a user')
         .addUserOption(opt =>
-            opt.setName('user').setDescription('User').setRequired(true))
+            opt.setName('user')
+                .setDescription('User')
+                .setRequired(true))
         .addNumberOption(opt =>
-            opt.setName('amount').setDescription('Amount in SOL').setRequired(true)),
+            opt.setName('amount')
+                .setDescription('Amount')
+                .setRequired(true)),
 
     async execute(interaction) {
+
+        if (interaction.user.id !== OWNER_ID) {
+            return interaction.reply({ content: '❌ You cannot use this.', ephemeral: true });
+        }
 
         const user = interaction.options.getUser('user');
         const amount = interaction.options.getNumber('amount');
 
-        if (amount <= 0) return interaction.reply('❌ Invalid amount.');
-
         addBalance(user.id, amount);
 
-        interaction.reply(`➕ Added ${formatSol(amount)} to ${user.username}`);
+        interaction.reply(`💰 Added ${formatSol(amount)} to ${user.username}`);
     }
 };
